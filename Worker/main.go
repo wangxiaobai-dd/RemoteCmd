3dbc32d53ae77e8ade24aaf072cfc816bd47c205
wangxiaobai-dd <leoeee@icloud.com>
2022-11-23 19:01:53 +0800

    commit

--- Worker/main.go
+++ Worker/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"os"
 )
 
 var headers = [...]string{"cmd.h", "cmd2.h"}
 
 func main() {
+
+	router := gin.Default()
+	router.POST("/serverInfo", postServerInfo)
+
 	fmt.Println("worker")
 	for _, header := range headers {
 		dealHeaderFile(header)
 	}
+}
+
+func postServerInfo(c *gin.Context) {
+
 }
 
 func dealHeaderFile(f string) {
